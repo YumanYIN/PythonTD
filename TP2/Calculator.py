fb26f9eb34ddda21c443b7c2a28a147b58d26301
Yin Developer <yinyuman96@gmail.com>
2020-09-21 12:36:48 +0200

    TP2-Calculator-YumanYIN

--- TP2/Calculator.py
+++ TP2/Calculator.py
@@ -1,111 +1,197 @@
 from tkinter import *
-#import tkFont
-import os
-from functools import partial
-from PIL import Image, ImageTk
+
+base_buttons = [
+    ['7', '8', '9', '+'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '*'],
+    ['0', '.', '=', '/'],
+    ['AC', 'C', '+/-', '%']
+]
+
+scientific_buttons = [
+    ['7', '8', '9', '+', '(', ')'],
+    ['4', '5', '6', '-', 'x^2', 'x^2'],
+    ['1', '2', '3', '*', 'x^y', '2^x'],
+    ['0', '.', '=', '/', 'x!', 'e'],
+    ['AC', 'C', '+/-', '%', '1/x', 'pi']
+]
+
+show_str = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '/', '*', '(', ')']
 
 
-def get_input(entry, argu):
-    entry.insert(END, argu)
+def center_window(root, w, h):
+    """
+    This function can center the window on the screen
+    :param root:
+    :param w: width of widget
+    :param h: height of widget
+    :return:
+    """
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 
-def backspace(entry):
-    input_len = len(entry.get())
-    entry.delete(input_len - 1)
+class Calculator:
+
+    def __init__(self, root):
+
+        self.root = root
+
+        self.root.title("My Calculator")
+        self.root.resizable(0, 0)
+        self.entry = Entry(self.root, justify="right")
+
+        # initialization string equation to do eval()
+        self.equation = ''
+
+        # This Boolean value determines whether to clear the Entry
+        self.newline = False
+
+        # Menu
+        self.menus()
+        # Center the window on the screen
+        # center_window(self.root, 200, 300)
+        self.base_layout()
+
+    def base_layout(self):
+        self.entry.grid(row=0, column=0, columnspan=4, sticky=N + W + S + E, padx=3, pady=3)
+
+        buttons = []
+
+        # Create Buttons
+        for i in range(5):
+            for j in range(4):
+                value = base_buttons[i][j]
+                button = self.creatButton(value)
+                buttons.append(button)
+
+        # Put buttons in Tk widget
+        count = 0
+        for row in range(1, 6):  # row 0 is used for Entry
+            for col in range(4):
+                buttons[count].grid(row=row, column=col)
+                count += 1
+
+    def scientific_layout(self):
+        self.entry.grid(row=0, column=0, columnspan=6, sticky=N + W + S + E, padx=3, pady=3)
+
+        buttons = []
+
+        # Create Buttons
+        for i in range(5):
+            for j in range(6):
+                value = scientific_buttons[i][j]
+                button = self.creatButton(value)
+                buttons.append(button)
+
+        # Put buttons in Tk widget
+        count = 0
+        for row in range(1, 6):  # row 0 is used for Entry
+            for col in range(6):
+                buttons[count].grid(row=row, column=col)
+                count += 1
+
+    def creatButton(self, value, show=True, width=3):
+        """
+
+        :param value:
+        :param show:
+        :param width:
+        :return:
+        """
+        if value not in show_str:
+            show = False
+
+        return Button(
+            self.root,
+            text=value,
+            command=lambda: self.click(value, show),
+            width=width)
+
+    def click(self, val, show):
+        if not self.newline:
+            if not show:
+                if val == '=' and self.equation:
+                    self.calculate()
+                elif val == "AC":
+                    self.clear_all()
+                elif val == "C":
+                    self.clear_last()
+                elif val == "+/-" and self.equation:
+                    self.pos_or_neg()
 
 
-def clear(entry):
-    entry.delete(0, END)
+            else:
 
+                self.input_entry(val)
+        else:
+            self.clear_all()
+            if show:
+                self.input_entry(val)
 
-def calc(entry):
-    input = entry.get()
-    output = str(eval(input.strip()))
-    clear(entry)
-    entry.insert(END, output)
+    def input_entry(self, value, newline=False):
+        self.entry.insert(END, value)
+        self.equation += str(value)
+        self.newline = newline
 
-def cal():
-    root = Tk()
-    root.title("Calc")
-    root.resizable(0, 0)
+    def calculate(self):
+        """
+        input = self.entry.get()
+        output = str(eval(input.strip()))
+        self.clear()
+        self.entry.insert(END, output)
+        """
+        try:
+            result = str(eval(self.equation))
 
-    menu = Menu(root)
-    filemenu = Menu(menu, tearoff=0)
-    filemenu.add_command(label="Base")
-    filemenu.add_command(label="Scientific")
-    menu.add_cascade(label="Type", menu=filemenu)
-    root.config(menu=menu)
+            self.clear_all()
+            # show result in Entry and make newline True
+            self.input_entry(result, newline=True)
 
-    #entry_font = tkFont.Font(size=12)
-    entry = Entry(root, justify="right")
-    entry.grid(row=0, column=0, columnspan=4, sticky=N + W + S + E, padx=5, pady=5)
+        except:
+            self.clear_all()
+            self.entry.insert(END, 'Error, click AC to restart')
 
-    #button_font = tkFont.Font(size=10, weight=tkFont.BOLD)
-    button_bg = '#D5E0EE'
-    button_active_bg = '#E5E35B'
+    def pos_or_neg(self):
+        pass
 
-    myButton = partial(Button, root, bg=button_bg, padx=10, pady=3, activebackground=button_active_bg)
+    def clear(self):
+        self.entry.delete(0, END)
 
-    button7 = myButton(text='7', command=lambda: get_input(entry, '7'))
-    button7.grid(row=1, column=0, pady=5)
+    def clear_all(self):
+        self.equation = ''
+        self.refresh()
 
-    button8 = myButton(text='8', command=lambda: get_input(entry, '8'))
-    button8.grid(row=1, column=1, pady=5)
+    def clear_last(self):
+        self.equation = self.equation[:-1]
+        self.refresh()
 
-    button9 = myButton(text='9', command=lambda: get_input(entry, '9'))
-    button9.grid(row=1, column=2, pady=5)
+    def refresh(self):
+        self.entry.delete(0, END)
+        self.entry.insert(END, self.equation)
 
-    button10 = myButton(text='+', command=lambda: get_input(entry, '+'))
-    button10.grid(row=1, column=3, pady=5)
-
-    button4 = myButton(text='4', command=lambda: get_input(entry, '4'))
-    button4.grid(row=2, column=0, pady=5)
-
-    button5 = myButton(text='5', command=lambda: get_input(entry, '5'))
-    button5.grid(row=2, column=1, pady=5)
-
-    button6 = myButton(text='6', command=lambda: get_input(entry, '6'))
-    button6.grid(row=2, column=2, pady=5)
-
-    button11 = myButton(text='-', command=lambda: get_input(entry, '-'))
-    button11.grid(row=2, column=3, pady=5)
-
-    button1 = myButton(text='1', command=lambda: get_input(entry, '1'))
-    button1.grid(row=3, column=0, pady=5)
-
-    button2 = myButton(text='2', command=lambda: get_input(entry, '2'))
-    button2.grid(row=3, column=1, pady=5)
-
-    button3 = myButton(text='3', command=lambda: get_input(entry, '3'))
-    button3.grid(row=3, column=2, pady=5)
-
-    button12 = myButton(text='*', command=lambda: get_input(entry, '*'))
-    button12.grid(row=3, column=3, pady=5)
-
-    button0 = myButton(text='0', command=lambda: get_input(entry, '0'))
-    button0.grid(row=4, column=0, columnspan=2, padx=3, pady=5, sticky=N + S + E + W)
-
-    button13 = myButton(text='.', command=lambda: get_input(entry, '.'))
-    button13.grid(row=4, column=2, pady=5)
-
-    button14 = Button(root, text='/', bg=button_bg, padx=10, pady=3,
-                      command=lambda: get_input(entry, '/'))
-    button14.grid(row=4, column=3, pady=5)
-
-    button15 = Button(root, text='C', bg=button_bg, padx=10, pady=3,
-                      command=lambda: backspace(entry), activebackground=button_active_bg)
-    button15.grid(row=5, column=0, pady=5)
-
-    button16 = Button(root, text='AC', bg=button_bg, padx=10, pady=3,
-                      command=lambda: clear(entry), activebackground=button_active_bg)
-    button16.grid(row=5, column=1, pady=5)
-
-    button17 = Button(root, text='=', bg=button_bg, padx=10, pady=3,
-                      command=lambda: calc(entry), activebackground=button_active_bg)
-    button17.grid(row=5, column=2, columnspan=2, padx=3, pady=5, sticky=N + S + E + W)
-
-    root.mainloop()
+    def menus(self):
+        """
+        This function defines the calculator's drop-down menu
+        :return:
+        """
+        main_menu = Menu(self.root)
+        type_menu = Menu(main_menu, tearoff=0)
+        type_menu.add_command(label="Base", command=lambda: self.base_layout())
+        type_menu.add_command(label="Scientific", command=lambda: self.scientific_layout())
+        help_menu = Menu(main_menu, tearoff=1)
+        help_menu.add_command(label="Operation")
+        main_menu.add_cascade(label="Type", menu=type_menu)
+        main_menu.add_cascade(label="Help", menu=help_menu)
+        self.root.config(menu=main_menu)
 
 
 if __name__ == '__main__':
-    cal()
+    root = Tk()
+    myCal = Calculator(root)
+
+    root.mainloop()
