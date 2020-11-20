1d1c849ce69b08412a8fa4f746e35fdf33d8c0e8
wangwei <neuwangwei96@gmail.com>
2020-11-20 18:11:31 +0100

    fix ants

--- TP8/Ant/Ant.py
+++ TP8/Ant/Ant.py
@@ -2,6 +2,9 @@ import random
 from collections import deque
 
 
+image_size = 100
+
+
 class Ant:
     def __init__(self, x, y, size, color_filed, color_followed, probability_move, type_move, probability_follow):
         self.x = int(x)
@@ -10,104 +13,44 @@ class Ant:
         self.type_move = type_move
         self.probability_move = convertProbability(probability_move)
         self.probability_follow = float(probability_follow)
-        self.directions = deque('forward backward left right'.split(' '))
-        self.direction = self.directions[random.range(0, 3)]
-        self.color_followed = color_followed
-        self.color_filed = color_filed
+        self.color_followed = string_to_rgb(color_followed)
+        self.color_filed = string_to_rgb(color_filed)
 
     def move(self):
         if self.type_move == 'Dd':
             self.moveDd()
-        if self.type_move == 'Do':
-            self.moveDo()
         else:
             print("Movement type unknown")
+
+    def moveLeft(self):
+        self.x -= 1
+        if self.x < 0:
+            self.x = image_size - 1
+
+    def moveRight(self):
+        self.x += 1
+        if self.x >= image_size:
+            self.x = 0
+
+    def moveStraight(self):
+        self.y += 1
+        if self.y >= image_size:
+            self.y = 0
 
     def moveDd(self):
         """
         Movement droit
         :return:
         """
-        movements = 'Left' * int(self.probability_move[0] * 100) \
-                    + 'Straight' * int(self.probability_move[1] * 100) \
-                    + 'Right' * int(self.probability_move[2] * 100)
-        choice = random.choice(movements)
-        if choice == 'Left':
-            if self.direction == 'forward':
-                self.x += -1
-            elif self.direction == 'backward':
-                self.x += 1
-            elif self.direction == 'left':
-                self.y += -1
-            elif self.direction == 'right':
-                self.y += 1
-            self.rotate_left()
-        elif choice == 'Right':
-            if self.direction == 'forward':
-                self.x += 1
-            elif self.direction == 'backward':
-                self.x += -1
-            elif self.direction == 'left':
-                self.y += 1
-            elif self.direction == 'right':
-                self.y += -1
-            self.rotate_right()
-        elif choice == 'Straight':
-            if self.direction == 'forward':
-                self.y += 1
-            elif self.direction == 'backward':
-                self.y += -1
-            elif self.direction == 'left':
-                self.x += -1
-            elif self.direction == 'right':
-                self.x += 1
-
-    def moveDo(self):
-        """
-        Movement oblique
-        :return:
-        """
-        movements = 'Left' * int(self.probability_move[0] * 100) \
-                    + 'Straight' * int(self.probability_move[1] * 100) \
-                    + 'Right' * int(self.probability_move[2] * 100)
-        choice = random.choice(movements)
-        if choice == 'Left':
-            if self.direction == 'forward':
-                self.x += -1
-                self.y += 1
-            elif self.direction == 'backward':
-                self.x += 1
-                self.y += -1
-            elif self.direction == 'left':
-                self.x += -1
-                self.y += -1
-            elif self.direction == 'right':
-                self.x += 1
-                self.y += 1
-            self.rotate_left()
-        elif choice == 'Right':
-            if self.direction == 'forward':
-                self.x += 1
-                self.y += 1
-            elif self.direction == 'backward':
-                self.x += -1
-                self.y += -1
-            elif self.direction == 'left':
-                self.x += -1
-                self.y += 1
-            elif self.direction == 'right':
-                self.x += 1
-                self.y += -1
-            self.rotate_right()
-        elif choice == 'Straight':
-            if self.direction == 'forward':
-                self.y += 1
-            elif self.direction == 'backward':
-                self.y += -1
-            elif self.direction == 'left':
-                self.x += -1
-            elif self.direction == 'right':
-                self.x += 1
+        # direction = ''
+        r = random.randint(0, 10000) / 10000
+        if r < self.probability_move[0]:
+            direction = "L"
+        elif r - self.probability_move[0] < self.probability_move[1]:
+            direction = "R"
+        else:
+            direction = "S"
+        return direction
 
     def rotate_left(self):
         pass
@@ -120,15 +63,14 @@ class Ant:
             return True
         else:
             return False
-        return False
 
     def calculate_luminance(self, color_neighbor):
-        luminance_neighbor = 0.2426 * string_to_rgb(color_neighbor)[0] \
-                             + 0.7152 * string_to_rgb(color_neighbor)[1] \
-                             + 0.0722 * string_to_rgb(color_neighbor)[2]
-        luminance = 0.2426 * string_to_rgb(self.color_filed)[0] \
-                    + 0.7152 * string_to_rgb(self.color_filed)[1] \
-                    + 0.0722 * string_to_rgb(self.color_filed)[2]
+        luminance_neighbor = 0.2426 * color_neighbor[0] \
+                             + 0.7152 * color_neighbor[1] \
+                             + 0.0722 * color_neighbor[2]
+        luminance = 0.2426 * self.color_filed[0] \
+                    + 0.7152 * self.color_filed[1] \
+                    + 0.0722 * self.color_filed[2]
         return abs(luminance_neighbor - luminance)
 """
 def rgb_to_hex(rgbString):
@@ -138,9 +80,12 @@ def rgb_to_hex(rgbString):
 """
 
 def string_to_rgb(rgbString):
-    rgb = deque(rgbString.sqlit(','))
+    rgb = deque(rgbString.split(','))
+    rgb = [int(rgb[0]), int(rgb[1]), int(rgb[2])]
     return rgb
 
 
 def convertProbability(probability_move):
-    return probability_move
+    pro_string = probability_move.split(',')
+    pro = [float(pro_string[0]), float(pro_string[1]), float(pro_string[2])]
+    return pro
--- TP8/Ant/Main_ant.py
+++ TP8/Ant/Main_ant.py
@@ -1,5 +1,15 @@
 import json
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import json
+import threading
+import random
+from Ant import Ant, image_size
+
+
+image = [[[255, 255, 255] for i in range(0, image_size)] for j in range(0, image_size)]
+
 
 def getNeighbor(matrix, x, y, iteration, neigbor):
     if iteration == 0:
@@ -66,3 +76,122 @@ class Main_ant:
     antsList = []
     rectangles = []
     # rectangles.append(plt.Rectangle(0, 0), )
+
+
+def draw(x, y):
+    global image
+    for m in range(x, 100):
+        image[m][y] = [0, 0, 0]
+
+
+def load_json():
+    with open("ants.json", "r") as fp:
+        data = json.load(fp)
+    ants_array = []
+    for ant in data['ants']:
+        ants_array.append(Ant(ant['x'],
+                              ant['y'],
+                              ant['size'],
+                              ant['color_filed'],
+                              ant['color_followed'],
+                              ant['proba_move'],
+                              ant['type_move'],
+                              ant['proba_follow']))
+    return ants_array
+
+
+"""
+ * to get 3 surrounding pixels
+ * x: x of ant position
+ * y: y of ant position
+"""
+def detect(x, y):
+    left_x = x - 1 if x > 0 else image_size - 1
+    left = [left_x, y]
+
+    right_x = x + 1 if x < image_size - 1 else 0
+    right = [right_x, y]
+
+    up_y = y + 1 if y < image_size - 1 else 0
+    up = [x, up_y]
+
+    return left, right, up
+
+
+"""
+ * to get which direction the ant will follow
+"""
+def follow(ant):
+    l, r, u = detect(ant.x, ant.y)
+    goL = ant.luminance(image[l[0]][l[1]])
+    goR = ant.luminance(image[r[0]][r[1]])
+    goU = ant.luminance(image[u[0]][u[1]])
+    if goL or goR or goU:
+        random_number = random.randint(0, 10000) / 10000
+        if random_number < ant.probability_follow:
+            if goL and (not goR and not goU):
+                return 'L'
+            elif goR and (not goL and not goU):
+                return 'R'
+            elif goU and (not goR and not goL):
+                return "S"
+            elif goL and goR and not goU:
+                random_number2 = random.randint(0, 10000) / 10000
+                if random_number2 < ant.probability_move[0]/(ant.probability_move[0] + ant.probability_move[1]):
+                    return 'L'
+                else:
+                    return 'R'
+            elif goR and goU and not goL:
+                random_number2 = random.randint(0, 10000) / 10000
+                if random_number2 < ant.probability_move[1] / (ant.probability_move[2] + ant.probability_move[1]):
+                    return 'R'
+                else:
+                    return 'S'
+            elif goL and goU and not goR:
+                random_number2 = random.randint(0, 10000) / 10000
+                if random_number2 < ant.probability_move[0] / (ant.probability_move[2] + ant.probability_move[0]):
+                    return 'L'
+                else:
+                    return 'S'
+            else:
+                return ant.moveDd()
+    return "None"
+
+
+def draw_ants(ant):
+    global image
+    for i in range(0, 10000):
+        d = follow(ant)
+        if d == "None":
+            direction = ant.moveDd()
+        else:
+            direction = d
+
+        if direction is 'L':
+            ant.moveLeft()
+        elif direction is 'R':
+            ant.moveRight()
+        else:
+            ant.moveStraight()
+        mutex.acquire()
+        image[ant.x][ant.y] = ant.color_filed
+        mutex.release()
+
+
+if __name__ == "__main__":
+    ants = load_json()
+
+    t = []
+    mutex = threading.Lock()
+
+    t2 = threading.Thread(target=draw_ants(ants[1]))
+    t1 = threading.Thread(target=draw_ants(ants[0]))
+
+    t2.start()
+    t1.start()
+
+    t1.join()
+    t2.join()
+
+    plt.imshow(image)
+    plt.show()
